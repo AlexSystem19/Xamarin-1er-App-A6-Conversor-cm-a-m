1320a11b65c14eebd1570aa84b2bb089d191acab
H | A6 <100012260+AlexSystem19@users.noreply.github.com>
2022-08-13 19:23:40 -0500

    Agregar archivos de proyecto.

---
+++ Conversor/Vistas/MenuPrincipal.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Conversor.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void btniniciar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Convertir());
+        }
+    }
+}
